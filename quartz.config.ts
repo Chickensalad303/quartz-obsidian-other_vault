8f833214a70d34c994535a3f79a519ce10ddb78b
Chickensalad303 <91982781+Chickensalad303@users.noreply.github.com>
2025-11-14 10:32:38 +0100

    Add HardLineBreaks plugin to quartz.config.ts
    Added HardLineBreaks plugin for better Obsidian compatibility.

--- quartz.config.ts
+++ quartz.config.ts
@@ -72,6 +72,7 @@ const config: QuartzConfig = {
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.HardLineBreaks(), /* for better obsidian compatibility */
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
